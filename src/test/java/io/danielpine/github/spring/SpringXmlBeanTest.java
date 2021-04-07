efa23a58a928bebd9d3b0b10b1b1f4c46face916
danielpine <danielpine@sina.com>
2021-04-07 09:22:30 +0800

    u

--- src/test/java/io/danielpine/github/spring/SpringXmlBeanTest.java
+++ src/test/java/io/danielpine/github/spring/SpringXmlBeanTest.java
@@ -2,6 +2,8 @@ package io.danielpine.github.spring;
 
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
@@ -13,4 +15,15 @@ public class SpringXmlBeanTest {
         TestBean myTestBean = (TestBean) bf.getBean("testBean");
         System.out.println(myTestBean.getName());
     }
+
+    @Test
+    public void xmlBeanFactoryTest2() {
+        System.out.println("Starting..");
+        DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(defaultListableBeanFactory);
+        xmlBeanDefinitionReader.loadBeanDefinitions(new ClassPathResource("spring-config.xml"));
+        TestBean myTestBean = (TestBean) defaultListableBeanFactory.getBean("testBean");
+        System.out.println(myTestBean.getName());
+    }
+
 }
